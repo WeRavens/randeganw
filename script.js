ac86ad22fbd2c971ea4466428b7de7828ab648dc
Dexter <ikfahkmas@gmail.com>
2025-07-10 16:54:04 +0700

    update-home

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let btn=document.getElementById("btn");
 let navbar=document.getElementById("navbar");
 
+// alert("website ini dibuat mobile first!, untuk dekstop dikemudian hari")
 btn.addEventListener("click", function() {
     if(navbar.style.display==="none"){
         navbar.style.display="flex"
